19506c3922260a85907babfc02e1bf961f0cccd8
MMORONTO <t2wycemo@gmail.com>
2022-07-05 17:55:30 -0500

    add base url & api key

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = '';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Trawl API');
 });
+
+// GET Product Details
+app.get('/products/:productId');
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
