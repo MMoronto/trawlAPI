12ac32385f74170f27c7cc02b2d3b33e45425679
MMORONTO <t2wycemo@gmail.com>
2022-07-05 13:39:54 -0500

    set up API ports, request&response functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = preocess.env.PORT || 5000;
+app.use(expresss.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Trawl API');
+});
+
+app.listen(PORT, () => {});
